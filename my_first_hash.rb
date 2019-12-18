4dfb7acb09b497303ff6625ba7f977192ddbe849
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-18 04:11:53 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,7 +21,8 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  shipping_manifest["oil paintings"]
 end
 
 def adding
